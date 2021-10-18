82420978fbbfcd2c3a23e95fc5fc6681f9e86e94
Christy C <91734372+Christykarunya@users.noreply.github.com>
2021-10-18 17:13:29 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,18 @@
+package week3.day2;
+
+public class BankInfo {
+	
+	public void saving() {
+		System.out.println("Total Savings information of the Bank");
+
+	}
+	public void fixed() {
+		System.out.println("Fixed deposit details of the Bank");
+	
+	}
+	public void deposit() {
+		System.out.println("Total Deposits info of the Bank");
+		
+	}
+
+}
